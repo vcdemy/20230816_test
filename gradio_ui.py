3b9507b993b8a9dfb3e29feaa9eae976a4151035
victorgau <victorgau@gmail.com>
2023-08-17 15:14:58 +0800

    Update files

--- gradio_ui.py
+++ gradio_ui.py
@@ -1,6 +1,13 @@
 import gradio as gr
 
-def hello(name):
-    return name
 
-demo = gr.Interface(hello, 'text', 'text')
+def greet(text: str) -> str:
+    return text
+
+
+demo = gr.Interface(
+    fn=greet,
+    inputs=gr.components.Textbox(label='Input'),
+    outputs=gr.components.Textbox(label='Output'),
+    allow_flagging='never'
+)
