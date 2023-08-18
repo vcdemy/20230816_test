88aee04816db2be0a81f7a11a78a76217e8675d3
victorgau <victorgau@gmail.com>
2023-08-18 10:43:57 +0800

    update app2.py

--- app2.py
+++ app2.py
@@ -3,4 +3,4 @@ import gradio as gr
 def hello(name):
     return f"Hello {name}!"
 
-gr.Interface(hello, 'text', 'text').launch()
+gr.Interface(hello, 'text', 'text').launch(server_port=5000, server_name="0.0.0.0")
